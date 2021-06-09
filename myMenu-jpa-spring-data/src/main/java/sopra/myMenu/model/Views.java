f1ebda3cd394e27293e4f9f12acb60fe998625bb
camille <c>
2021-06-09 13:59:17 +0200

    rest-controller 1er jet

---
+++ myMenu-jpa-spring-data/src/main/java/sopra/myMenu/model/Views.java
@@ -0,0 +1,13 @@
+package sopra.myMenu.model;
+
+public class Views {
+	public static class ViewCommon{}
+	
+	public static class ViewListeCourse extends ViewCommon{}
+	
+	public static class ViewListeCourseWithAjustement extends ViewListeCourse{}
+	
+	public static class ViewMagasin extends ViewCommon{}
+	
+	public static class ViewMagasinWithMagasinIngredient extends ViewMagasin{}
+}
--- myMenu-jpa-spring-data/src/main/java/sopra/myMenu/repository/IListeCourseRepository.java
+++ myMenu-jpa-spring-data/src/main/java/sopra/myMenu/repository/IListeCourseRepository.java
@@ -1,10 +1,20 @@
 package sopra.myMenu.repository;
 
-import org.springframework.data.annotation.Version;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import sopra.myMenu.model.ListeCourse;
 
 public interface IListeCourseRepository extends JpaRepository<ListeCourse,Long> {
+	
+	@Query("select distinct l from ListeCourse l left join fetch l.ajustementQuantite")
+	List<ListeCourse> findAllWithAjustement();
 
+	@Query("select distinct l from ListeCourse l left join fetch l.ajustementQuantite where l.id = :id")
+	Optional<ListeCourse> findByIdWithAjustement(@Param("id") Long id);
+	
 }
